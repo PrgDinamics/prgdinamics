df7fdc2acd73969f80b7cd94f30812166ed4d6f7
PrgDinamics <proyectodianitanet@gmail.com>
2025-12-03 21:31:56 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true } 
+  images: {
+    unoptimized: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "intranet.lcsofttestings.xyz"
+          }
+        ],
+        destination: "/dynedu/:path*"
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
